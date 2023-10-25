f6f8124494c8e3f887d907ff0f9d6d3791190e6d
MkGee <matthewrmcgeehan2@gmail.com>
2023-10-25 10:21:39 -0400

    Add files via upload

---
+++ codebug_dictionary.py
@@ -0,0 +1,26 @@
+student_data = (
+    'Jason': 'Gryffindor',
+    'Kaitlynne': 'Ravenclaw',
+    'Aidan': 'Slytherin',
+    'Nicole': 'Hufflepuff',
+    'Antonio': 'Gryffindor',
+    'Alexander': 'Ravenclaw',
+    'Samantha': 'Slytherin',
+    'Reagan': 'Hufflepuff',
+    'Lauren': 'Gryffindor',
+    'Emma': 'Ravenclaw',
+    'Veronica': 'Slytherin',
+    'Matthew': 'Hufflepuff',
+    'Madison': 'Gryffindor',
+    'Jijoy': 'Ravenclaw',
+    'Christopher': 'Slytherin',
+    'Alex': 'Hufflepuff',
+    'Cedric': 'Gryffindor',
+    'Jason': 'Ravenclaw'
+)
+
+# Print only the students in Gryffindor
+print("Students in Gryffindor:")
+for student, house in student_data.items():
+    if house = 'Gryfindor':
+        print(student)
